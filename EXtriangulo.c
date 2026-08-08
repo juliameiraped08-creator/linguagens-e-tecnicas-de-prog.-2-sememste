eb48c43d6aa07cd0cd29491b2e4ca4d1a2fc06c1
juliameiraped08-creator <juliameiraped08@gmail.com>
2026-08-08 19:47:34 -0300

    Add EXtriangulo.c to calculate triangle area
    Implement a program to calculate the area of a right triangle using base and height.

---
+++ EXtriangulo.c
@@ -0,0 +1,15 @@
+#include <stdio.h> #include <stdlib.h>
+
+int main () {
+
+int base, altura;
+float area;
+
+base = 10;
+altura = 20;
+
+area = (base*altura)/2;
+printf("a area do triagulo retangulo de base = %d e altura= %d e %f",altura,area, base);
+
+return 0;
+}
